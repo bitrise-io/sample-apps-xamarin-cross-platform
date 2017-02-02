fb45de32a03f210537d04c06cb8be66aba16e83e
Krisztian Godrei <godreikrisztian@gmail.com>
2017-02-02 16:53:33 +0100

    xamarin ios uitest

---
+++ Multiplatform.iOS.UITest/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.iOS;
+using Xamarin.UITest.Queries;
+
+namespace Multiplatform.iOS.UITest
+{
+	[TestFixture]
+	public class Tests
+	{
+		iOSApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			string appBundlePath = Environment.GetEnvironmentVariable("APP_BUNDLE_PATH");
+
+			if (appBundlePath != null && appBundlePath != "")
+			{
+				// In case of Bitrise step: steps-xamarin-ios-uitest.
+				app = ConfigureApp
+					.iOS
+					.AppBundle(appBundlePath)
+					.StartApp();
+			}
+			else {
+				// In case of Bitrise step: steps-xamarin-testcloud-for-ios or running in the IDE.
+				app = ConfigureApp
+					.iOS
+					.StartApp();
+			}
+		}
+
+		[Test]
+		public void AppLaunches()
+		{
+			app.Screenshot("First screen.");
+		}
+	}
+}
